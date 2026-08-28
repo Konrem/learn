f49032eadfe68e862f92dd8127cbf8dda29e88ef
Артем Кондратенко <lord,kazakovv@gmail.com>
2026-08-28 14:26:08 +0300

    feat(react): add controlled stakeholder form, validate and submit form

---
+++ apps/react-lab/src/modules/10-forms/StakeholderFormPage.tsx
@@ -0,0 +1,9 @@
+import StakeholderForm from "./StakeholderForm";
+
+export default function StakeholderFormPage() {
+  return (
+    <div>
+      <StakeholderForm />
+    </div>
+  );
+}
---
+++ apps/react-lab/src/modules/10-forms/types.ts
@@ -0,0 +1,7 @@
+export interface StakeholderFormValues {
+    name: string;
+    email: string;
+    role: string;
+    influence: string;
+    notes: string;
+  };
